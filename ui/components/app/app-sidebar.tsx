1204db8de2a8068f84b06079a86d52e92f859492
Tejas Venugopal <tejas0908@gmail.com>
2024-11-24 01:17:34 +0530

    added nextjs ui skeleton

---
+++ ui/components/app/app-sidebar.tsx
@@ -0,0 +1,45 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+
+const items = [
+  {
+    title: "Projects",
+    url: "/dashboard/projects",
+    icon: Home,
+  }
+]
+
+export function AppSidebar() {
+    return (
+      <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+    )
+}
